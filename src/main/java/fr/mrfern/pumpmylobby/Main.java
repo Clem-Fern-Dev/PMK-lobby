544669f59de6be2aea93d8b0b8d45712ed8411b7
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-09 17:43:48 +0100

    Add geter serverInfo

--- src/main/java/fr/mrfern/pumpmylobby/Main.java
+++ src/main/java/fr/mrfern/pumpmylobby/Main.java
@@ -1,22 +1,41 @@
 package fr.mrfern.pumpmylobby;
 
+import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import fr.mrfern.pumpmylobby.porg.MisterPorg;
 
 public class Main extends JavaPlugin {
 
-	@Override
-	public void onEnable() {
-		// TODO Auto-generated method stub
-	}
+	private static MisterPorg misterP;
+	private static Server server;
 	
 	@Override
 	public void onLoad() {
-		// TODO Auto-generated method stub
+		setMisterP(new MisterPorg(this, "MzgyNTc4Mzg4MDY3NTQ5MTg0.DQdApA.zxYqzecf2pn3HMt6rRZGbcibggs","387326167499276292"));
+		server = this.getServer();
 	}
+	
+	@Override
+		public void onEnable() {
+			// TODO Auto-generated method stub
+		}
 	
 	@Override
 	public void onDisable() {
 		// TODO Auto-generated method stub
+	}
+
+	public static MisterPorg getMisterP() {
+		return misterP;
+	}
+
+	public void setMisterP(MisterPorg misterP) {
+		Main.misterP = misterP;
+	}
+	
+	public static Server getServerInfo() {
+		return server;
 	}
 	
 }
