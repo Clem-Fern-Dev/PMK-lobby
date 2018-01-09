ecadfd5d34ce06494479a439e6b87baac3d6993f
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-09 17:35:18 +0100

    Ajout Main class

---
+++ src/main/java/fr/mrfern/pumpmylobby/Main.java
@@ -0,0 +1,22 @@
+package fr.mrfern.pumpmylobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void onLoad() {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+	}
+	
+}
