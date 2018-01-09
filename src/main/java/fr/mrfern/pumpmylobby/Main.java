79e9071c1a1e6cd9ffac12eda2e893a70d762b06
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-09 17:54:21 +0100

    Ajout methods d'appelle Porg event

--- src/main/java/fr/mrfern/pumpmylobby/Main.java
+++ src/main/java/fr/mrfern/pumpmylobby/Main.java
@@ -4,38 +4,36 @@ import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.mrfern.pumpmylobby.porg.MisterPorg;
+import fr.mrfern.pumpmylobby.porg.PorgServerEvent;
+
 
 public class Main extends JavaPlugin {
 
-	private static MisterPorg misterP;
 	private static Server server;
+	private static Main main;
 	
 	@Override
 	public void onLoad() {
-		setMisterP(new MisterPorg(this, "MzgyNTc4Mzg4MDY3NTQ5MTg0.DQdApA.zxYqzecf2pn3HMt6rRZGbcibggs","387326167499276292"));
 		server = this.getServer();
+		main = this;
 	}
 	
 	@Override
-		public void onEnable() {
-			// TODO Auto-generated method stub
-		}
+	public void onEnable() {
+		new PorgServerEvent().OnServerStartEvent(new MisterPorg(this, "MzgyNTc4Mzg4MDY3NTQ5MTg0.DQdApA.zxYqzecf2pn3HMt6rRZGbcibggs","387326167499276292"));	
+	}
 	
 	@Override
 	public void onDisable() {
-		// TODO Auto-generated method stub
-	}
-
-	public static MisterPorg getMisterP() {
-		return misterP;
-	}
-
-	public void setMisterP(MisterPorg misterP) {
-		Main.misterP = misterP;
+		new PorgServerEvent().OnServerStopEvent(new MisterPorg(this, "MzgyNTc4Mzg4MDY3NTQ5MTg0.DQdApA.zxYqzecf2pn3HMt6rRZGbcibggs","387326167499276292"));
 	}
 	
 	public static Server getServerInfo() {
 		return server;
+	}
+
+	public static Main getMain() {
+		return main;
 	}
 	
 }
