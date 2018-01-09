8c0e92ade4ea9f7e9c21206822f7fe983b3228d3
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-09 20:45:59 +0100

    Fix get server name

--- src/main/java/fr/mrfern/pumpmylobby/porg/PorgServerEvent.java
+++ src/main/java/fr/mrfern/pumpmylobby/porg/PorgServerEvent.java
@@ -7,7 +7,7 @@ public class PorgServerEvent {
 	public void OnServerStartEvent(MisterPorg misterP) {
 		
 		PorgTextChannel porgChan = misterP.getPorgTextChannel();		
-		porgChan.sendPorgMessage("@everyone **" + Main.getServerInfo().getName() + " est en ligne et opérationnel ! **\nRejoingnez nous en vous connectant via pumpmykins.eu").complete();
+		porgChan.sendPorgMessage("@everyone **" + Main.getServerInfo().getServerName() + " est en ligne et opérationnel ! **\nRejoingnez nous en vous connectant via pumpmykins.eu").complete();
 
 		misterP.close();
 	}
@@ -15,7 +15,7 @@ public class PorgServerEvent {
 	public void OnServerStopEvent(MisterPorg misterP) {
 		
 		PorgTextChannel porgChan = misterP.getPorgTextChannel();		
-		porgChan.sendPorgMessage("@everyone **" + Main.getServerInfo().getName() + " est hors-ligne !** \nIl sera à nouveau disponible dans les plus brefs délais. Restez informé des nouveautés et mises à jours sur le forum/discord/site !").complete();
+		porgChan.sendPorgMessage("@everyone **" + Main.getServerInfo().getServerName() + " est hors-ligne !** \nIl sera à nouveau disponible dans les plus brefs délais. Restez informé des nouveautés et mises à jours sur le forum/discord/site !").complete();
 	
 		misterP.close();
 	}
