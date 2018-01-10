4c37bf09b613059ada11b6d38903271bb3648c95
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-10 11:22:21 +0100

    Fix message

--- src/main/java/fr/mrfern/pumpmylobby/porg/PorgServerEvent.java
+++ src/main/java/fr/mrfern/pumpmylobby/porg/PorgServerEvent.java
@@ -13,7 +13,7 @@ public class PorgServerEvent {
 		PorgTextChannel porgChan = misterP.getPorgTextChannel();
 		
 		if(config.getBoolean("discord.debug_mod")) {
-			porgChan.sendPorgMessage(new MessageBuilder().append(misterP.getJda().getTextChannelById("375790951081181187")).append(" __debug_mod__**" + Main.getServerInfo().getServerName() + " est en ligne et opérationnel ! ** \nRejoingnez nous en vous connectant via pumpmykins.eu").build())
+			porgChan.sendPorgMessage(new MessageBuilder().append(misterP.getJda().getTextChannelById("375790951081181187")).append(" __debug_mod__ **" + Main.getServerInfo().getServerName() + " est en ligne et opérationnel ! ** \nRejoingnez nous en vous connectant via pumpmykins.eu").build())
 			.complete();
 		}else {					
 			porgChan.sendPorgMessage("@everyone **" + Main.getServerInfo().getServerName() + " est en ligne et opérationnel ! **\nRejoingnez nous en vous connectant via pumpmykins.eu").complete();
@@ -27,7 +27,7 @@ public class PorgServerEvent {
 		PorgTextChannel porgChan = misterP.getPorgTextChannel();
 		
 		if(config.getBoolean("discord.debug_mod")) {
-			porgChan.sendPorgMessage(new MessageBuilder().append(misterP.getJda().getTextChannelById("375790951081181187")).append(" __debug_mod__**" + Main.getServerInfo().getServerName() + " est hors-ligne !** \nIl sera à nouveau disponible dans les plus brefs délais. Restez informé des nouveautés et mises à jours sur le forum/discord/site !").build())
+			porgChan.sendPorgMessage(new MessageBuilder().append(misterP.getJda().getTextChannelById("375790951081181187")).append(" __debug_mod__ **" + Main.getServerInfo().getServerName() + " est hors-ligne !** \nIl sera à nouveau disponible dans les plus brefs délais. Restez informé des nouveautés et mises à jours sur le forum/discord/site !").build())
 			.complete();
 		}else {					
 			porgChan.sendPorgMessage("@everyone **" + Main.getServerInfo().getServerName() + " est hors-ligne !** \nIl sera à nouveau disponible dans les plus brefs délais. Restez informé des nouveautés et mises à jours sur le forum/discord/site !").complete();
