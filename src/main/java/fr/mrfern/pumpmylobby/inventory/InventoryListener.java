5b4ff161414b3e7080b81c90b36ba2b0f7af723d
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-10 13:47:13 +0100

    fix drop item event

--- src/main/java/fr/mrfern/pumpmylobby/inventory/InventoryListener.java
+++ src/main/java/fr/mrfern/pumpmylobby/inventory/InventoryListener.java
@@ -1,10 +1,13 @@
 package fr.mrfern.pumpmylobby.inventory;
 
-import org.bukkit.Color;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -14,8 +17,12 @@ public class InventoryListener implements Listener {
 
 	@EventHandler
 	public void OnJoinSetInv(PlayerJoinEvent e) {
+		
+		System.out.println("PlayerJoinEvent call");
+		
 		Player p = e.getPlayer();	
 		Inventory inv = p.getInventory();
+		NavBarManager m = new NavBarManager(p);
 		
 		inv.clear();
 		
@@ -23,35 +30,44 @@ public class InventoryListener implements Listener {
 			ItemStack navItemRagna1 = new ItemStack(Material.APPLE);
 			ItemMeta navItemRagna1Meta = navItemRagna1.getItemMeta();
 						
-			navItemRagna1Meta.setDisplayName(Color.PURPLE + "PumpMyRagna#1");
+			navItemRagna1Meta.setDisplayName(ChatColor.LIGHT_PURPLE + "PumpMyRagna#1");
 			navItemRagna1.setItemMeta(navItemRagna1Meta);
 			
 			inv.setItem(2, navItemRagna1);
+		}else {
+			
 		}
 		
 		if(p.hasPermission("server.dev")) {
 			ItemStack navItemDev = new ItemStack(Material.BEDROCK);
 			ItemMeta navItemDevMeta = navItemDev.getItemMeta();
 			
-			navItemDevMeta.setDisplayName(Color.YELLOW + "development_server");
+			navItemDevMeta.setDisplayName(ChatColor.YELLOW + "development_server");
 			navItemDev.setItemMeta(navItemDevMeta);
 			
 			inv.setItem(4,navItemDev);
+		}else {
+			
 		}
 		
 		if(p.hasPermission("server.ragna2")) {
-			ItemStack navItemRagna2 = new ItemStack(Material.CARROT);
+			ItemStack navItemRagna2 = new ItemStack(Material.BREAD);
 			ItemMeta navItemRagna2Meta = navItemRagna2.getItemMeta();
 			
-			navItemRagna2Meta.setDisplayName(Color.FUCHSIA + "PumpMyRagna#2");
+			navItemRagna2Meta.setDisplayName(ChatColor.AQUA + "PumpMyRagna#2");
 			navItemRagna2.setItemMeta(navItemRagna2Meta);
 			
 			inv.setItem(6, navItemRagna2);
+		}else {
+			
 		}
 	}
 	
-	public void OnCancelModifInv() {
-		
+	@EventHandler
+	public void OnCancelDropInv(PlayerDropItemEvent e) {
+		if(!e.getPlayer().getGameMode().equals(GameMode.CREATIVE) & !e.getPlayer().getGameMode().equals(GameMode.SPECTATOR)) {
+			e.setCancelled(true);
+		}
 	}
 	
 }
