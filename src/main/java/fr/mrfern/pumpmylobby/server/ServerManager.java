73c4f2ff24e174f2f62414b2878a965c0472691c
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-11 13:39:21 +0100

    Ajout get ServerState

--- src/main/java/fr/mrfern/pumpmylobby/server/ServerManager.java
+++ src/main/java/fr/mrfern/pumpmylobby/server/ServerManager.java
@@ -2,12 +2,20 @@ package fr.mrfern.pumpmylobby.server;
 
 import java.util.HashMap;
 
+import org.bukkit.entity.Player;
+
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+
+import fr.mrfern.pumpmylobby.Main;
+
 public class ServerManager {
 	
+	private static ServerManager serverManager = new ServerManager();
 	private static HashMap<String, ServerData> serverHash;
+	private Player p;
 
 	public static void initConfig() {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -17,6 +25,25 @@ public class ServerManager {
 
 	public static void setServerHash(HashMap<String, ServerData> serverHash) {
 		ServerManager.serverHash = serverHash;
+	}
+
+	public static ServerManager getManager(Player p) {
+		serverManager.setP(p);
+		return serverManager;
+	}
+
+	public boolean getServerState(String serverName) {
+		
+		
+		return false;
+	}
+
+	public Player getP() {
+		return p;
+	}
+
+	public void setP(Player p) {
+		this.p = p;
 	}
 
 }
