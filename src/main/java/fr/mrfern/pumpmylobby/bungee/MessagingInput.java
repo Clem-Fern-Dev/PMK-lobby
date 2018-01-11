9078af4e928df521288755391e6ca053d34dcd2f
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-11 13:38:33 +0100

    AJout class de reception

---
+++ src/main/java/fr/mrfern/pumpmylobby/bungee/MessagingInput.java
@@ -0,0 +1,13 @@
+package fr.mrfern.pumpmylobby.bungee;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+public class MessagingInput implements PluginMessageListener {
+
+	@Override
+	public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+		
+	}
+
+}
---
+++ src/main/java/fr/mrfern/pumpmylobby/inventory/MessageManager.java
@@ -0,0 +1,36 @@
+package fr.mrfern.pumpmylobby.inventory;
+
+import java.util.Map.Entry;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import fr.mrfern.pumpmylobby.server.ServerData;
+import fr.mrfern.pumpmylobby.server.ServerManager;
+
+public class MessageManager {
+
+	private Player player;
+	
+	public MessageManager(Player p) {
+		player = p;
+	}
+
+	public void OnJoin(ServerManager serverManager) {
+		player.sendMessage(ChatColor.GOLD + "[ PumpMyLobby ] Bonjour " + ChatColor.BOLD + ChatColor.YELLOW +player.getName());
+		for (Entry<String, ServerData> entry : ServerManager.getServerHash().entrySet()) {
+			if(serverManager.getServerState(entry.getKey())) {
+				
+			}
+		}
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+}
