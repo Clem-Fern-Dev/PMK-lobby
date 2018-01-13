c6468b7023045189ba3475fcf80287609f91948a
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-13 19:54:49 +0100

    AJout com & fix null buffer return

--- src/main/java/fr/mrfern/pumpmylobby/bungee/Requete.java
+++ src/main/java/fr/mrfern/pumpmylobby/bungee/Requete.java
@@ -13,36 +13,43 @@ public class Requete {
 	}
 
 	public static Requete PrejoinReq(Player sen,String serverName) {
-		
-		
+		// génération du buffer	de demande si en ligne ou non, et si ban
 		
 		return new Requete(sen, "BungeeCord" , "".getBytes());		
 	}
 	
 	public static void OnPreJoinReqOK(Player sen) {
+		// si offline alors erreur et return + update inv
 		
+		//sinon si ban alors return + send message ban + infos
+		
+		// sinon check nombre de joueurs avec JoinReq
 	}
 	
 	public static Requete joinReq(Player sen, String serverName) {
-		
-		
+		// génération du buffer de demande si en ligne ou non et get nombre de joueur		
 		
 		return new Requete(sen,serverName, "".getBytes());			
 	}
 	
 	public static void OnJoinReqOK() {
+		// si offline alors erreur et return + update inv
+		
+		// sinon check si autorisé à rejoindre avec le nombre de joueur
+		
+		// si autorisé alors connect avec ConnectReq
 		
 	}
 	
 	public static Requete ConnectReq(Player sen, String serverName) {
-		
-		
+		// génération du buffer de demande de connection		
 		
 		return new Requete(sen,serverName, "".getBytes());		
 	}
 	
+	@Deprecated
 	public static void OnConnectReqOk(Player sen, String serverName) {
-		
+	
 	}
 
 
