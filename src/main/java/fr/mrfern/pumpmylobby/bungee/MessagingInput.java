3371bd9b37ffb953cd99dda49d4a598433cb1342
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-14 13:14:49 +0100

    AJout detection reception + get tag + get subchannel + dispatche requete & send response

--- src/main/java/fr/mrfern/pumpmylobby/bungee/MessagingInput.java
+++ src/main/java/fr/mrfern/pumpmylobby/bungee/MessagingInput.java
@@ -3,11 +3,34 @@ package fr.mrfern.pumpmylobby.bungee;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteStreams;
+
 public class MessagingInput implements PluginMessageListener {
 
 	@Override
 	public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+		System.out.println(channel);
+		if (channel.equals("BungeeCord")) {
+			System.out.println("Bungee cord channel");
+			ByteArrayDataInput in = ByteStreams.newDataInput(message);
+			
+		    String subchannel = in.readUTF();
+		    
+		    if (subchannel.equals("prejoinresponse")) {
+		    	System.out.println("response");
+		    	String serverName = in.readUTF();
+		    	boolean serverState = in.readBoolean();
+		    	
+		    	if(!serverState) {
+		    		System.out.println("serverstate false");
+		    		Requete.OnPreJoinResp(player, serverName, serverState, null);
+		    	}
+		    	System.out.println("serverstate true");
+		    	
+		    }
+			
+		}		
 		
 	}
-
 }
