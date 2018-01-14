74d4b035fccd0c3bbfc526d8f596487a7bc8d3b1
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-14 13:13:51 +0100

    FIx import & test implement PluginMessageEvent

--- src/main/java/fr/mrfern/pumpmylobby/Main.java
+++ src/main/java/fr/mrfern/pumpmylobby/Main.java
@@ -1,7 +1,9 @@
 package fr.mrfern.pumpmylobby;
 
 import org.bukkit.Server;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
 
 import fr.mrfern.pumpmylobby.bungee.MessagingInput;
 import fr.mrfern.pumpmylobby.inventory.InventoryListener;
