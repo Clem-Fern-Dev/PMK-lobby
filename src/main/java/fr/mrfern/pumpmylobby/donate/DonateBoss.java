0bbc87d2ee161a0ea11dd42041da2a38c1030b27
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-02-06 23:45:30 +0100

    AJout class DonateBoss & ajout method init

---
+++ src/main/java/fr/mrfern/pumpmylobby/donate/DonateBoss.java
@@ -0,0 +1,16 @@
+package fr.mrfern.pumpmylobby.donate;
+
+import org.bukkit.Location;
+import org.bukkit.plugin.Plugin;
+
+public class DonateBoss {
+	
+	public static void DonateEnable(Plugin plg, Location location) {
+		
+	}
+
+	public static void initDonateHolo() {
+		// TODO Auto-generated method stub
+		
+	}
+}
