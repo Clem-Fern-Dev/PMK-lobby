6eacb3e9bb4064499c52f601e12c615c8ba0e726
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-02-07 22:45:03 +0100

    Ajout methods init & disableDonate + fix initHologram

--- src/main/java/fr/mrfern/pumpmylobby/donate/DonateBoss.java
+++ src/main/java/fr/mrfern/pumpmylobby/donate/DonateBoss.java
@@ -1,5 +1,7 @@
 package fr.mrfern.pumpmylobby.donate;
 
+import java.util.UUID;
+
 import org.bukkit.Location;
 
 import com.sainttx.holograms.api.Hologram;
@@ -7,29 +9,45 @@ import com.sainttx.holograms.api.HologramManager;
 import com.sainttx.holograms.api.line.HologramLine;
 import com.sainttx.holograms.api.line.TextLine;
 
+import fr.mrfern.pumpmylobby.Main;
+
 public class DonateBoss {
 	
 	private static HologramManager holoManager;
+	
+	private static Location holoLoc;
+	private static Hologram holo;
 
 	public static void donateEnable(HologramManager hologramManager) {
-		holoManager = hologramManager;		
+		holoManager = hologramManager;	
+		holoLoc = new Location(Main.getServerInfo().getWorld(""), 0, 0,0);
 	}
 	
-	public void createHologram(String id, Location location) {
+	public static void donateDisable(Hologram hologram) {
+		hideHologram(hologram);
+		deleteHologram(hologram);
+	}
+	
+	public static void InitHologram(Hologram holo) {
+		holo = createHologram(UUID.randomUUID().toString(), holoLoc);
+	}
+	
+	public static Hologram createHologram(String id, Location location) {
 	    Hologram hologram = new Hologram(id, location);
 	    holoManager.addActiveHologram(hologram); // Tells the plugin a new Hologram was added
+	    return hologram;
 	}
 	
-	public void addTextLine(Hologram hologram, String text) {
+	public static void addTextLine(Hologram hologram, String text) {
 	    HologramLine line = new TextLine(hologram, text);
 	    hologram.addLine(line);
 	}
 	
-	public void deleteHologram(Hologram hologram) {
+	public static void deleteHologram(Hologram hologram) {
 	    holoManager.deleteHologram(hologram);
 	}
 	
-	public void hideHologram(Hologram hologram) {
+	public static void hideHologram(Hologram hologram) {
 	    hologram.despawn();
 	    holoManager.removeActiveHologram(hologram);
 	}
@@ -40,5 +58,13 @@ public class DonateBoss {
 
 	public static void setHoloManager(HologramManager holoManager) {
 		DonateBoss.holoManager = holoManager;
+	}
+
+	public static Hologram getHolo() {
+		return holo;
+	}
+
+	public static void setHolo(Hologram holo) {
+		DonateBoss.holo = holo;
 	}
 }
