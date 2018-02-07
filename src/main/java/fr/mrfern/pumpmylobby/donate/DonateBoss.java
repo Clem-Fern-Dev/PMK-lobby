c31cb1628568c4c45c2a3ad2996c41e86c3c0534
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-02-07 20:07:58 +0100

    AJout getter holograms plugin class

--- src/main/java/fr/mrfern/pumpmylobby/donate/DonateBoss.java
+++ src/main/java/fr/mrfern/pumpmylobby/donate/DonateBoss.java
@@ -1,16 +1,25 @@
 package fr.mrfern.pumpmylobby.donate;
 
-import org.bukkit.Location;
-import org.bukkit.plugin.Plugin;
+import com.sainttx.holograms.api.HologramManager;
 
 public class DonateBoss {
 	
-	public static void DonateEnable(Plugin plg, Location location) {
-		
-	}
+	private static HologramManager holoManager;
 
 	public static void initDonateHolo() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public static void donateEnable(HologramManager hologramManager) {
+		holoManager = hologramManager;		
+	}
+
+	public static HologramManager getHoloManager() {
+		return holoManager;
+	}
+
+	public static void setHoloManager(HologramManager holoManager) {
+		DonateBoss.holoManager = holoManager;
 	}
 }
