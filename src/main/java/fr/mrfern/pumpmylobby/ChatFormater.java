433c8702b2712577dcb5e28f5bc19c98ebdde516
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-02-18 20:48:11 +0100

    AJout event Chat & ajout methods init

--- src/main/java/fr/mrfern/pumpmylobby/ChatFormater.java
+++ src/main/java/fr/mrfern/pumpmylobby/ChatFormater.java
@@ -1,9 +1,44 @@
 package fr.mrfern.pumpmylobby;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import me.lucko.luckperms.api.LuckPermsApi;
 
 public class ChatFormater implements Listener{
 
+	private static RegisteredServiceProvider<LuckPermsApi> provider;
 	
+	public static void initChatFormateur() {
+		
+		
+		
+	}
+	
+	@EventHandler
+	public void OnChatMessage(AsyncPlayerChatEvent e) {
+	
+		Player p = e.getPlayer();
+		
+		if(isMute(e.getPlayer())) {
+			// cancel message
+			
+		}else {
+			// set format
+			String format;
+			
+			
+			
+		}
+		
+	}
+
+	private boolean isMute(Player player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
 }
