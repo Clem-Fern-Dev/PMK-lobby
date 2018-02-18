cc24047d56f42e9ced78af5d7220d907b1bd1321
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-02-18 23:58:34 +0100

    Fix import & ajout récupération des préfixs

--- src/main/java/fr/mrfern/pumpmylobby/ChatFormater.java
+++ src/main/java/fr/mrfern/pumpmylobby/ChatFormater.java
@@ -1,36 +1,83 @@
 package fr.mrfern.pumpmylobby;
 
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
+import me.lucko.luckperms.api.Contexts;
 import me.lucko.luckperms.api.LuckPermsApi;
+import me.lucko.luckperms.api.User;
+import me.lucko.luckperms.api.caching.MetaData;
+import me.lucko.luckperms.api.caching.UserData;
 
 public class ChatFormater implements Listener{
 
 	private static RegisteredServiceProvider<LuckPermsApi> provider;
+	private static LuckPermsApi api;
+	
+	private static boolean apiState;
 	
 	public static void initChatFormateur() {
 		
+		provider = Bukkit.getServicesManager().getRegistration(LuckPermsApi.class);	// récupération du provider
 		
+		if (provider != null) {	// test du provider
+		    api = provider.getProvider();	// récupération de l'api
+		    setApiState(true);	// state ok
+		}else {
+			setApiState(false);		// state non ok
+			try {
+				throw new IOException("LuckPerms API error accès !");	// throw Exception
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 	}
 	
 	@EventHandler
 	public void OnChatMessage(AsyncPlayerChatEvent e) {
-	
-		Player p = e.getPlayer();
+		if(!apiState)	// si API non dispo
+			return;		// alors return
 		
-		if(isMute(e.getPlayer())) {
+		
+		Player p = e.getPlayer(); // récupération de l'instance player
+		
+		if(isMute(e.getPlayer())) {	// test du mute du player 
 			// cancel message
 			
 		}else {
-			// set format
+			// instanciation format format
 			String format;
 			
-			
+			UUID playerUuid = p.getUniqueId();	//récupération de l'uuid
+					User user = api.getUser(playerUuid);	// récupération de l'User luckPerm API
+					if (user == null) {		//cheack du load de l'user
+					    // user load error
+					} else {
+					    
+						UserData uData = user.getCachedData();
+						Optional<Contexts> contexts = api.getContextForUser(user);
+						MetaData metaData = uData.getMetaData(contexts.get());
+						
+						format = ">";
+						
+						for (Entry<Integer, String> entry : metaData.getPrefixes().entrySet()) {
+							format += " [" + entry.getValue() + "]";
+						}
+						
+						e.setFormat(format);
+						
+					}
 			
 		}
 		
@@ -47,6 +94,22 @@ public class ChatFormater implements Listener{
 
 	public static void setProvider(RegisteredServiceProvider<LuckPermsApi> provider) {
 		ChatFormater.provider = provider;
+	}
+
+	public static LuckPermsApi getApi() {
+		return api;
+	}
+
+	public static void setApi(LuckPermsApi api) {
+		ChatFormater.api = api;
+	}
+
+	public static boolean isApiState() {
+		return apiState;
+	}
+
+	public static void setApiState(boolean apiState) {
+		ChatFormater.apiState = apiState;
 	}
 	
 }
